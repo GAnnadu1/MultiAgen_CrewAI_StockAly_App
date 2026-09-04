f59f09d9a17c812ff38247f17ec84c95b797c379
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-09-04 13:46:32 +0200

    Update LLM model from gemini-3.1 to gemini-omni-1.1

--- agents/trader_agent.py
+++ agents/trader_agent.py
@@ -4,7 +4,8 @@ from tools.stock_research_tools import get_stock_price
 
 #initialize LLM
 llm = LLM(
-    model="gemini/gemini-3.1-flash-lite",
+    model="gemini/gemini-omni-1.1-flash",
+    api_key=st.secrets["GEMINI_API_KEY"],
     temperature=0
     )
 
