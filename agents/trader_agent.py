51be0706e5775325c763ce7e5a4bd7fa45780232
GAnnadu1 <126003327+GAnnadu1@users.noreply.github.com>
2026-09-04 12:59:29 +0200

    Implement trader agent with LLM and stock tools

---
+++ agents/trader_agent.py
@@ -0,0 +1,20 @@
+from crewai import Agent, LLM
+from tools.stock_research_tools import get_stock_price
+
+#initialize LLM
+llm = LLM(
+    model="gemini/gemini-3.1-flash-lite",
+    api_key=os.environ["GEMINI_API_KEY"],
+    temperature=0
+    )
+
+trader_agent = Agent(
+    role="Strategic Stock Trader",
+    goal=("Formulate investment strategies and execute trades based on comprehensive market analysis,"
+          "aiming for optimal portfolio growth and risk management."),
+    backstory=("You are a seasoned investment strategist with a track record of identifying profitable"
+               "trading opportunities and managing risk effectively across diverse market conditions."),
+    llm=llm,
+    tools=[get_stock_price], # Including get_stock_price as a relevant tool for market data
+    verbose=True
+)
